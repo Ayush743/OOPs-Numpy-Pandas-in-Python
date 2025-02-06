785a25923a5bfd9d745bcfdbeabc55cf6c3aa9f4
Ayush743 <ayush210042@gmail.com>
2025-02-06 10:34:45 +0530

    adding inheritance and encapsulation concepts

--- OOPs/inheritance.py
+++ OOPs/inheritance.py
@@ -16,3 +16,39 @@ car1.start_engine()
 
 truck1=Truck()
 truck1.start_engine()
+
+
+class Animal:
+    all=[]
+    def __init__(self,name,species):
+        self.name=name
+        self.species=species
+        Animal.all.append(self)
+
+    def make_sound(self):
+        print("I make sound")
+    def desc(self):
+        print(f"My name is {self.name},i am a {self.species}")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name} || {self.species})"
+    
+        
+class Dog(Animal):
+    
+    def __init__(self, name, species,no_of_legs):
+        super().__init__(name, species) # we have access to all the methods and variables created in the parent class
+        self.num_legs=no_of_legs
+        
+
+    def make_sound(self):
+        print("I bark")
+    def desc(self):
+        print(f"My name is {self.name},i am a {self.species} and i have {self.num_legs} legs")
+
+dog1=Dog("Tommy","Dog",4)
+cat1=Animal("Kitty","cat")
+dog1.desc()
+dog1.make_sound()
+cat1.desc()
+print(dog1.all)
